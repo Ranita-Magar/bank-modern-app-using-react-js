2ac0c67de67cf0fc8a04d6966f4cb66aca1e0d18
Ranita-Magar <87315393+Ranita-Magar@users.noreply.github.com>
2024-05-28 14:54:13 +0545

    Creating responsive Navbar & Hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
         <img
           src={toggle ? close : menu}
           alt="menu"
-          className="w-[28px] h-[28px] object-contain "
+          className="w-[28px] h-[28px] object-contain cursor-pointer"
           onClick={() => setToggle((prev) => !prev)}
         />
       </div>
@@ -38,7 +38,7 @@ const Navbar = () => {
           toggle ? "flex" : "hidden"
         } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
       >
-        <ul className="list-none flex flex-col justify-end items-center flex-1">
+        <ul className="list-none flex flex-col justify-end items-center flex-1 ">
           {navLinks.map((nav, index) => (
             <li
               key={nav.id}
